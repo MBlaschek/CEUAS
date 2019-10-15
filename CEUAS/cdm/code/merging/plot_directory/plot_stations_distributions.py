42c6adef172b0c003af654f5888ecc6f10709b63
Federico Ambrogi <federico.ambrogi88@gmail.com>
2019-10-15 14:48:31 +0200

    up plots

--- CEUAS/cdm/code/merging/plot_directory/plot_stations_distributions.py
+++ CEUAS/cdm/code/merging/plot_directory/plot_stations_distributions.py
@@ -29,25 +29,33 @@ def plot_properties(flag = ''):
         gl.ylabels_right = False        
         gl=ax.gridlines(draw_labels=True , ls = ':', color = 'lightgray')
         plt.grid(ls = '--', color = 'lightgray')  
-        '''
-        
+        '''        
         ax.stock_img()        
-
-        ax.coastlines(color = 'blue')
 
 
     elif flag == 'pretty': 
         ax = plt.axes(projection=ccrs.Mollweide())
         ax.stock_img()
-        ax.coastlines()
+
+    ax.coastlines(color = 'black' , linewidth=0.3)
 
     #ax.coastline()
     #plt.xlim([-180.,180.])
     #plt.ylim([-90.,90.])
 
-       
-    d = {'ncar':'blue', 'bufr':'lime', 'igra2':'cyan', 'era5_1':'magenta', 'era5_1759':'yellow', 'era5_1761':'black', 'era5_3188':'orange' } 
-    #d = {'ncar':'blue', } 
+    """ dictionary defining the style of the points """   
+    d = {'ncar'     : ['darkorange' , 4 , 'o'] , 
+         'bufr'     : ['red'    , 3 , 'o'] ,
+         'igra2'    : ['mediumpurple'   , 6 , 'o'] ,
+         'era5_1'   : ['lime' , 3  , 'o'] , 
+         'era5_1759': ['blue'   , 13 , 'o'] , 
+         'era5_1761': ['cyan'   , 8 , 'o'] ,
+         'era5_3188': ['yellow' , 2 , 'o'] ,
+ } 
+
+
+
+
     
     return d
 
@@ -83,13 +91,17 @@ def makePlot(start_date= '', end_date = '', file = '', flag = ''):
     """ Main function to plot maps. 
          [start, end]: time range for the data 
          file+ summary of all the station_configuration files merged together """
-    
+
+    print('Plotting :::' , start_date, ' ' , end_date , ' ' , flag )
+
+    # check the column names order in the merging_netcdf.py script     
     col_names = ['ncar_lat', 'ncar_lon', 'ncar_start', 'ncar_end',    
                              'igra2_lat', 'igra2_lon', 'igra2_start', 'igra2_end'  ,    
                              'bufr_lat', 'bufr_lon', 'bufr_start', 'bufr_end', 
-                             'era5_1761_lat', 'era5_1761_lon', 'era5_1761_start', 'era5_1761_end',     
+     
                              'era5_1_lat', 'era5_1_lon', 'era5_1_start', 'era5_1_end',      
                              'era5_1759_lat', 'era5_1759_lon', 'era5_1759_start', 'era5_1759_end', 
+                             'era5_1761_lat', 'era5_1761_lon', 'era5_1761_start', 'era5_1761_end',
                              'era5_3188_lat', 'era5_3188_lon', 'era5_3188_start', 'era5_3188_end' ]
                     
 
@@ -98,15 +110,18 @@ def makePlot(start_date= '', end_date = '', file = '', flag = ''):
        
     dic = plot_properties(flag = flag)
     
-    for d, color  in dic.items():
+    for d in ['era5_1759', 'era5_1761', 'igra2', 'ncar', 'era5_3188', 'bufr', 'era5_1']:
+
+        lat, lon =  clean_data( tab[ d + '_lat'], tab[d + '_lon'],  tab[d + '_start'] , tab[d + '_end'] , date_min = start_date, date_max= end_date )  # cleaning the station to be plotted                                                                                    
+
+        if len(lon) <1: continue 
+
+        color  = dic[d][0]
+        marker = dic[d][2]
+        size   = dic[d][1]
         lab = d.replace('era5_1','ERA5 1').replace('era5_3188','ERA5 3188').replace('era5_1759','ERA5 1759').replace('era5_1761','ERA5 1761').replace('igra2','IGRA2').replace('ncar','NCAR').replace('bufr','BUFR')         
-        lat, lon =  clean_data( tab[ d + '_lat'], tab[d + '_lon'],  tab[d + '_start'] , tab[d + '_end'] , date_min = start_date, date_max= end_date )  # cleaning the station to be plotted 
-        
-        #lat, lon = lat[:10], lon[:10]
-        #lat = [-5, 100]
-        #lon = [100, -50]
-        #plt.scatter (lon, lat,   color = color , transform=ccrs.PlateCarree(central_longitude = 0.0), s = 7, label = d)
-        plt.scatter (lon, lat,   color = color, transform=ccrs.PlateCarree(central_longitude = 0.0), s = 4, label = lab + ' [' + str(len(lon)) + ']' )
+
+        plt.scatter (lon, lat,   color = color, transform=ccrs.PlateCarree(central_longitude = 0.0), s = size, label = lab + ' [' + str(len(lon)) + ']' )
        
     plt.title('Data Availability from ' + start_date + ' to ' + end_date , y = 1.04 )   
     plt.legend(loc = 'lower left', ncol = 1, fontsize = 9)   
@@ -119,7 +134,10 @@ def makePlot(start_date= '', end_date = '', file = '', flag = ''):
 ############################# Plotting part       
        
 for f in ['', 'pretty']:
-        
+    
+    a = makePlot(start_date = '1940-01-01', end_date = '1950-01-01',   file = 'summary_forplot.dat', flag = f )
+   
+     
     a = makePlot(start_date = '1900-01-01', end_date = '1920-01-01',   file = 'summary_forplot.dat', flag = f )
     a = makePlot(start_date = '1920-01-01', end_date = '1940-01-01',   file = 'summary_forplot.dat', flag = f ) 
     a = makePlot(start_date = '1940-01-01', end_date = '1950-01-01',   file = 'summary_forplot.dat', flag = f ) 
