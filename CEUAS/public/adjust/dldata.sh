e185394ee949838713421aefec24f45380fb4b36
Ulrich_Voggenberger <a1400070@srvx8.img.univie.ac.at>
2021-04-29 14:49:35 +0200

    removed old files

--- CEUAS/public/adjust/dldata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /etc/profile.d/modules.sh
-module load anaconda3/2020.07-gcc-8.3.1
-module load netcdf-fortran/4.5.3-intel-20.0.2
-module load intel-parallel-studio/composer.2020.2-intel-20.0.2
-# unlimit stacksize
-
-cd Converters
-python3 from_cds_to_legacy.py
