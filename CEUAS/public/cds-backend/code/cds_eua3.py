3ca9fdef4d0f07873eb031544a86964321684ccf
Ihr Name <ihre@emailadresse.de>
2021-06-25 15:29:10 +0200

    obs dim fix

--- CEUAS/public/cds-backend/code/cds_eua3.py
+++ CEUAS/public/cds-backend/code/cds_eua3.py
@@ -2590,7 +2590,7 @@ class CDMDataset:
                                          'original_precision', 'reference_sensor_id', 'report_id','data_policy_licence']+cdm_obstab)
                 # 'observed_variable','units'
                 logger.debug('Group %s copied [%5.2f s]', igroup, time.time() - time0)
-                fout['time'].make_scale('time')
+#                 fout['time'].make_scale('time')
             #
             # Feedback Information
             #
