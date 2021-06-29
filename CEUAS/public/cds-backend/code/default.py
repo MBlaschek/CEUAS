7f895f8cee81beae12f995c2800e0aaf9bc478d2
Ihr Name <ihre@emailadresse.de>
2021-06-29 08:29:06 +0200

    constraints fix

--- CEUAS/public/cds-backend/code/default.py
+++ CEUAS/public/cds-backend/code/default.py
@@ -1700,7 +1700,7 @@ def index(request=None, response=None):
     logger.debug("GET %s", request.query_string)
 
 
-    rfile='/data/public/constraints.csv'
+    rfile='/tmp/constraints.csv'
 
     response.set_header('Content-Disposition', 'attachment; filename=' + os.path.basename(rfile))
     return rfile
