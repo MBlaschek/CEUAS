e5dce8c61eef226264717ea212d011e475459701
Ihr Name <ihre@emailadresse.de>
2021-10-28 13:09:28 +0200

    faster version

--- CEUAS/public/testing/calc_fiduceo.py
+++ CEUAS/public/testing/calc_fiduceo.py
@@ -36,49 +36,91 @@ def haversine_np(lon1, lat1, lon2, lat2):
     return km
 
 
-def calc_station(date, search_lat, search_lon, channel):
+def calc_station(date, search_lat, search_lon, channel, stat):
     yr = date[0]
     mn = date[1]
     print(yr, mn)
-    folder = glob.glob('/users/staff/a1400070/CEUAS/CEUAS/public/testing/fiduceo/n16/dap.ceda.ac.uk/neodc/fiduceo/data/fcdr/microwave/v4.1/amsub/noaa16/'+str(yr)+'/'+"%02d" % (mn)+'/*/')
-    counter = 0
-    mean = 0
-    for i in folder:
+    folder = glob.glob('/users/staff/a1400070/CEUAS/CEUAS/public/testing/fiduceo/dap.ceda.ac.uk/neodc/fiduceo/data/fcdr/microwave/v4.1/mhs/noaa19/'+str(yr)+'/'+"%02d" % (mn)+'/*/')
+#     folder = glob.glob('/users/staff/a1400070/CEUAS/CEUAS/public/testing/fiduceo/dap.ceda.ac.uk/neodc/fiduceo/data/fcdr/microwave/v4.1/amsub/noaa15/'+str(yr)+'/'+"%02d" % (mn)+'/*/')
+#     folder = glob.glob('/users/staff/a1400070/CEUAS/CEUAS/public/testing/fiduceo/n16/dap.ceda.ac.uk/neodc/fiduceo/data/fcdr/microwave/v4.1/amsub/noaa16/'+str(yr)+'/'+"%02d" % (mn)+'/*/')
+    counter = [0]*len(stat)
+    mean = [0]*len(stat)
+    times = [[]]*len(stat)
+    dists = [[]]*len(stat)
+    for i in folder: # for each day of selected month
         try:
             files = glob.glob(i+'*.nc')
             df_night = None
             df_day = None
+
+    #         ds = xarray.open_mfdataset(files, combine='nested') 
+    #         # ValueError: arguments without labels along dimension 'y' cannot be aligned because they have different dimension sizes: {2304, 2303}
+
             for j in files:
-                start = (j.split('_')[-5][-6:-4])
                 ds = xarray.open_dataset(j)
-                df = ds[channel].to_dataframe().dropna()
+                df = ds[[channel,'Time']].to_dataframe().dropna()
                 df_day = df.append(df_day)
-            dist_day = haversine_np(len(df_day)*[search_lon], len(df_day)*[search_lat], df_day.longitude, df_day.latitude)
-            mean = mean + df_day[dist_day == dist_day.min()][channel].values
-            counter = counter + 1
+                
+            for k in range(len(stat)):
+                dist_day = haversine_np(len(df_day)*[search_lon[k]], len(df_day)*[search_lat[k]], df_day.longitude, df_day.latitude)
+                mindist = dist_day.min()
+                selection = df_day[dist_day == mindist]
+                mean[k] = mean[k] + selection[channel].values
+                times[k].append(selection.Time.values)
+                dists[k].append(mindist)
+                counter[k] = counter[k] + 1
         except: pass
-    try:
-        mean = (mean/counter)
-    except:
-        mean = np.nan
-    pickle.dump( mean, open( "./fiduceo/fiduceo_noaa16_"+str(chan)+"_"+str(yr)+"_"+"%02d" % (mn)+"_"+str(lat)+"_"+str(lon)+"_brightness_temperature.p", "wb" ) )
+    for k in range(len(stat)):
+        try:
+            out_mean = (mean[k]/counter[k])
+            out_times = times[k]
+            out_dists = dists[k]
+        except:
+            out_mean = np.nan
+            out_times = [np.nan]
+            out_dists = [np.nan]
+            
+        try:
+            os.makedirs("./fiduceo/out/all_noaa19/"+stat)
+        except:
+            pass
+        pickle.dump( [out_mean, out_times, out_dists], open( "./fiduceo/out/all_noaa19/"+stat+"/fiduceo_mhs_noaa19_"+str(chan)+"_"+str(yr)+"_"+"%02d" % (mn)+"_"+str(lat)+"_"+str(lon)+"_brightness_temperature.p", "wb" ) )
+    
+    
+#     pickle.dump( mean, open( "./fiduceo/fiduceo_amsub_noaa15_"+str(chan)+"_"+str(yr)+"_"+"%02d" % (mn)+"_"+str(lat)+"_"+str(lon)+"_brightness_temperature.p", "wb" ) )
+#     pickle.dump( mean, open( "./fiduceo/fiduceo_amsub_noaa16_"+str(chan)+"_"+str(yr)+"_"+"%02d" % (mn)+"_"+str(lat)+"_"+str(lon)+"_brightness_temperature.p", "wb" ) )
 
 
 if __name__ == '__main__': 
-    lat = 48.2
-    lon = 16.37
-    chan = 'Ch20_BT'
+#     selstats = glob.glob('/users/staff/a1400070/CEUAS/CEUAS/public/testing/rttov_out_hum_noaa_19_mhs/*')
+#     stats = []
+#     for i in selstats:
+#         stats.append(glob.glob('/mnt/users/scratch/leo/scratch/converted_v7/*'+ i.split('/')[-1] +'*_CEUAS_merged_v1.nc')[0])
+        
+#     lats = []
+#     lons = []
+#     names = []
+#     for i in stats:
+#         names.append(i.split('-')[-1][:5])
+#         df = eua.CDMDataset(filename = i).to_dataframe(groups=['observations_table'], variables=['latitude', 'longitude'])
+#         lats.append(df.latitude.iloc[0])
+#         lons.append(df.longitude.iloc[0])
+#     print('lat, lon collected')
+    
     try:
         os.makedirs("./fiduceo/")
     except:
         pass
     
     dates = []
-    for yr in range(1999, 2012, 1):
+    for yr in range(2009, 2018, 1):
         for mn in range(1, 13, 1):
             dates.append([yr, mn])
+    print(dates[20])
 
-    pool = multiprocessing.Pool(processes=30)
-    func=partial(calc_station, search_lat = lat, search_lon = lon, channel = chan)
-    result_list = list(pool.map(func, dates))
+    lats, lons, names = pickle.load(open("./stationlist.p", "rb" ))
+    chan = 'Ch3_BT'
+    pool = multiprocessing.Pool(processes=10)
+    func=partial(calc_station, search_lat = lats, search_lon = lons, channel = chan, stat = names)
+    result_list = list(pool.map(func, dates[20:21]))
     print(result_list)
