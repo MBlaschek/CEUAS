3a06154e23ac550419f68bdf3724b3fa38ef4c25
Ihr Name <ulrich.voggenberger@gmx.at>
2022-04-08 12:21:18 +0200

    fixed single_parallel - so automatically use it with only one station

--- CEUAS/public/cds-backend/code/default.py
+++ CEUAS/public/cds-backend/code/default.py
@@ -1235,8 +1235,8 @@ def check_body(variable: list = None, statid: list = None, product_type: str = N
                 'stations ')
     d['statid'] = statid
     print(len(d['statid']), d['statid'])
-#     if len(d['statid']) == 1:
-#         d['single_parallel'] = True
+    if len(d['statid']) == 1:
+        d['single_parallel'] = True
     #
     #
     # remove statids i
@@ -1244,12 +1244,12 @@ def check_body(variable: list = None, statid: list = None, product_type: str = N
 #     #
 #     # single_parallel
 #     #
-#     if (single_parallel == False) or (single_parallel == 'False'):
-#         d['single_parallel'] = False
-    if (single_parallel == True) or (single_parallel == 'True'):
-        d['single_parallel'] = True
-    else:
+    if (single_parallel == False) or (single_parallel == 'False') or (len(d['statid']) > 1):
         d['single_parallel'] = False
+#     if (single_parallel == True) or (single_parallel == 'True'):
+#         d['single_parallel'] = True
+#     else:
+#         d['single_parallel'] = False
     
     #
     #
