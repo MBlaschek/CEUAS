5c6576521c1eb112b4d90018162531b95d7b78b0
fambrogi <federico.ambrogi88@gmail.com>
2023-01-05 12:46:27 +0100

    Fixed reading processed stations in run harvester

--- CEUAS/public/harvest/code_cop2/run_harvest_convert_to_netCDF.py
+++ CEUAS/public/harvest/code_cop2/run_harvest_convert_to_netCDF.py
@@ -80,6 +80,8 @@ datasets = ['era5_1_mobile']
 
 datasets = all_ds
 
+datasets = [ 'era5_1']
+
 """ Check processed files """
 check_missing = True
 REDO = False
@@ -109,7 +111,7 @@ if not os.path.isdir('logs/'):
 for d in datasets:
        processed_dir = out_dir + '/' + d 
        print ('DATASET IS', d )
-       if 'era5_1_' not in d:
+       if d not in ['era5_1', 'era5_1_mobile', 'era5_2', 'era5_2_mobile']:
               files_list = [ db[d]+ '/' + f for f in os.listdir(db[d]) if os.path.isfile( db[d]+'/'+f ) ] # extracting the \
               f_list = [ f for f in files_list if os.path.getsize(f) > 1 ] # cleaning the list of files in the original database directories                                                               
               f_list = filelist_cleaner(f_list, d = d)
@@ -121,8 +123,10 @@ for d in datasets:
        else:
               processes = 10 # !!! there is already the pool splitting in the harvester, cannot be higher due to memory issues 
               # era5.conv.??????.82930.txt.gz
-
-              if not os.path.isfile('logs/' + d + '_files_list.txt'):  ### TODO, this is to speed ud reading the file names which take a lot of time
+              if not os.path.isdir('files_list'):
+                     os.mkdir('files_list')
+                     
+              if not os.path.isfile('files_list/' + d + '_files_list.txt'):  ### TODO, this is to speed ud reading the file names which take a lot of time
                      if d == 'era5_1':
                             flist=glob.glob('logs/' + db[d] + "/era5.conv._*") # takes too long
                      elif d == 'era5_1_mobile':
@@ -130,33 +134,45 @@ for d in datasets:
                             flist = glob.glob(db[d] + "/era5.conv.??????._*")  
                             flist = np.unique( ['._' + f.split('._')[1] for f in flist if '_' in f  ])
                      #flist =[f for f in flist if '_40179' not in f  and '42147' not in f] # this file causes problems ???
-                     a = open( 'logs/' + d + '_files_list.txt','w')
+                     a = open( 'files_list/' + d + '_files_list.txt','w')
                      for l in flist:
                             a.write(l + '\n')
                      a.close()
                 
               else:
-                     flist = [ f.replace('\n','') for f in open(d + '_files_list.txt').readlines() ]
+                     a = open('files_list/' + d + '_files_list.txt', 'r').readlines()
+                     flist = [ f.replace('\n','') for f in a]
 
-              odbs = flist
+              #odbs = flist
+              #stat = [f.split('._')[1] for f in odbs ] # numerical codes for station e.g. '07630'
+              
+              # create names of merged files to check in the directory 
+              harvestable_stations = ['era5.conv.??????.' + s.split('._')[1] + '.txt.gz.nc'   for s in flist ]
 
-              stat = [f.split('._')[1] for f in odbs ]
+              if check_missing:
+                     processed = [s.split('_harvested_')[1] for s in os.listdir(processed_dir) if 'harvested' in s]
+                     f_list = [f for f in harvestable_stations if f not in processed ]
+              else:
+                     f_list = flist
+                     
+              f_list = ['"' + db[d] + '/' + s.replace('.nc', '')+ '"' for s in f_list ] # adjust names with "" symbols and remove .nc extension
+               
+              """
               if d == 'era5_1_mobile':
                      f_list = ['"' + db[d] + '/era5.conv.??????._' + s.replace('.txt.gz', '') + '.txt.gz' + '"' for s in stat ]
               else:
                      f_list = ['"' + db[d] + '/era5.conv.??????.' + s.replace('.txt.gz', '') + '.txt.gz' + '"' for s in stat ]
               if check_missing:
                      try:
-                            processed = [s.split('_harvested_')[1] for s in os.listdir(processed_dir) if 'harvested' in s]
+                            processed = [s.split('_harvested_')[1].replace('.??????.','._').replace('.txt.gz.nc','') for s in os.listdir(processed_dir) if 'harvested' in s]
                             if d == 'era5_1':
-                                   
                                    f_list = [f for f in f_list if f.split('/1/')[1].replace('"','')+'.nc' not in processed ]
                             else:
                                    f_list = [f for f in f_list if f.split('/1_mobile/')[1].replace('"','')+'.nc' not in processed ]
-                                   
                      except:
                             pass
-
+              """
+              
        # filter failed 
        # filt = [ f.replace('\n','') for f in open('logs/' + d + '_failed.txt').readlines() ]
        # f_list = [f for f in f_list if f not in filt ]
@@ -166,13 +182,17 @@ for d in datasets:
               f_list = [f for f in f_list if '_' in f ]
               # done to filter out aggregated yearly files...
        print("FLIST::: \n",   f_list )
+       
+       # harvest random files per process 
        import random
        random.shuffle(f_list)
+       
+       # divide into chunks
        chunks = chunk_it(f_list, processes)
        print('+++++++++ TOTAL NUMBER OF FILES to be reprocessed ::: ', len(f_list) )
        for c in chunks:
               print ('*** I am running CHUNK: ', chunks.index(c) , ' *** with: ' ,  len(c), ' files'  )
               c = str(','.join(c)).replace('#','')
-              #os.system('python3.8  harvest_convert_to_netCDF.py  -d ' + d + ' -o ' + out_dir + ' -f ' + c + ' & ')                                                                              
+              os.system('python3.8  harvest_convert_to_netCDF.py  -d ' + d + ' -o ' + out_dir + ' -f ' + c + ' & ')                                                                              
 
 print('*** Finished with the parallel running ***')
