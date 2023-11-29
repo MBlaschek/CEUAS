faede297baf5c19f2df703210e2ae06b1432317c
Ihr Name <ulrich.voggenberger@gmx.at>
2023-11-29 09:28:37 +0100

    added main for import

--- CEUAS/public/resort/rasotools-master/rasotools/additions/vapformulae.py
+++ CEUAS/public/resort/rasotools-master/rasotools/additions/vapformulae.py
@@ -55,31 +55,31 @@ def fdpd(T,ew,rh):
        p1 = p
        q1 = mukodiff(p1,e)
        
+if __name__ == "__main__":
+    T=numpy.arange(1000)/10.+233.15
+    ew=numpy.log(muko(T))
 
-T=numpy.arange(1000)/10.+233.15
-ew=numpy.log(muko(T))
+    #plt.plot(T,muko(T))
+    print(muko(273.15))
+    #print dpd(T,0.4)
+    #plt.show()
+    rh=numpy.arange(96)+5
+    rh=numpy.log(rh/100.)
+    dpds=numpy.empty((T.shape[0],rh.shape[0]))
+    n=10
+    for irh in range(0,rh.shape[0]):
+    print(irh)
+    for it in range(0,T.shape[0]):
+        dpds[it,irh]=fdpd(T[it],ew[it],rh[irh])
 
-#plt.plot(T,muko(T))
-print(muko(273.15))
-#print dpd(T,0.4)
-#plt.show()
-rh=numpy.arange(96)+5
-rh=numpy.log(rh/100.)
-dpds=numpy.empty((T.shape[0],rh.shape[0]))
-n=10
-for irh in range(0,rh.shape[0]):
-   print(irh)
-   for it in range(0,T.shape[0]):
-      dpds[it,irh]=fdpd(T[it],ew[it],rh[irh])
-
-plt.subplot(1,2,1)
-rh=numpy.exp(rh)
-plt.contourf(rh,T,dpds,levels=numpy.arange(400)/10.)
-plt.colorbar()
-plt.subplot(1,2,2)
-plt.plot(rh,dpds[600])
-plt.plot(rh,dpds[300])
-plt.plot(rh,dpds[200])
-plt.show()
-print('')
+    plt.subplot(1,2,1)
+    rh=numpy.exp(rh)
+    plt.contourf(rh,T,dpds,levels=numpy.arange(400)/10.)
+    plt.colorbar()
+    plt.subplot(1,2,2)
+    plt.plot(rh,dpds[600])
+    plt.plot(rh,dpds[300])
+    plt.plot(rh,dpds[200])
+    plt.show()
+    print('')
       
