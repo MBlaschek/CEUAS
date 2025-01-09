43398c6da90b112a0ec528916240aa07eff8e6be
Ihr Name <ulrich.voggenberger@gmx.at>
2025-01-09 12:06:07 +0100

    update with attrs

--- CEUAS/public/testing/CUON_add_missing_groups.py
+++ CEUAS/public/testing/CUON_add_missing_groups.py
@@ -5,10 +5,10 @@ import os, glob, sys
 
 sys.path.append(os.getcwd()+'/../cds-backend/code/')
 os.environ['PYTHONPATH'] = os.getcwd()+'/../cds-backend/code/'
-from harvest_convert_to_netCDF import write_dict_h5
+from harvest_convert_to_netCDF import write_dict_h5, load_cdm_tables
 
 
-def check_and_fix_file(file):
+def check_and_fix_file(file, attr_dict):
     """
     Find missing advanced_homogenisation groups and missing variables in it.
     """
@@ -24,17 +24,29 @@ def check_and_fix_file(file):
         else:
             return 0
 
+
     for missing_var in missing_vars:
         alldict = pd.DataFrame({missing_var:ov_vars})
-        write_dict_h5(file, alldict, 'advanced_homogenisation', {missing_var: { 'compression': 'gzip' } }, [missing_var])
+        write_dict_h5(file, alldict, 'advanced_homogenisation', {missing_var: { 'compression': 'gzip' } }, [missing_var], mode='a', attrs=attr_dict)
     return 1
 
 
 path = '/mnt/users/scratch/leo/scratch/converted_v25/long/'
 path = '/mnt/users/scratch/uvoggenberger/test_add_group/'
 all_files = glob.glob(path + "*.nc")
+attr_file = glob.glob(path + "*11035*.nc")
+
+target_vars = ['RAOBCORE_bias_estimate', 'RASE_bias_estimate', 'RICH_bias_estimate', 'RISE_bias_estimate', 'humidity_bias_estimate', 'wind_bias_estimate']
+attr_dict = {}
+with h5py.File(attr_file[0],  "r") as f:
+    for tv in target_vars:
+        attr_dict[tv] = {}
+        for atr in f['advanced_homogenisation'][tv].attrs:
+            if atr != 'DIMENSION_LIST':
+                attr_dict[tv][atr] = f['advanced_homogenisation'][tv].attrs[atr]
+
 
 for file in all_files:
-    result = check_and_fix_file(file)
+    result = check_and_fix_file(file, attr_dict)
     if result == 1:
         print(file, ": advanced_homogenisation has been added!")
