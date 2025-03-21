247ae8a7259dea39055e262c1a15174e15cac06c
Ihr Name <ulrich.voggenberger@gmx.at>
2025-03-21 13:50:11 +0100

    update

--- CEUAS/public/harvest/code_cop2/run_harvest_convert_to_netCDF_yearSplit.py
+++ CEUAS/public/harvest/code_cop2/run_harvest_convert_to_netCDF_yearSplit.py
@@ -1,10 +1,10 @@
 import glob
-
-from harvester_yearsplit_parameters import *  # datasets
+import sys
 from run_harvester_yearsplit_utils import *
 from tqdm import tqdm
 import random
 from random import shuffle
+import argparse
 #from check_correct import *
 
 '''
@@ -21,6 +21,41 @@ REMEMBER
 - era5_2, despite having those files, can be run using directly the global text file 
 '''
 
+parser = argparse.ArgumentParser(description="Crete station configuration table")
+
+parser.add_argument('--station_config_dir' , '-s', 
+                    help="Chose station configuration directory"  ,
+                    type = str,
+                    default = '.' )
+
+parser.add_argument('--parameter_file' , '-p', 
+                    help="Chose parameter file"  ,
+                    type = str,
+                    default = '.' )
+
+parser.add_argument('--ceuas_dir' , '-c', 
+                    help="Chose scrip directory"  ,
+                    type = str,
+                    default = '/srvfs/home/uvoggenberger/CEUAS/CEUAS/' )
+
+
+
+args = parser.parse_args()
+station_config_dir = args.station_config_dir
+parameter_file = args.parameter_file
+ceuas_dir = args.ceuas_dir
+
+if parameter_file != '.': 
+       print(parameter_file)
+       sys.path.append(parameter_file.split('modded')[0])
+       from modded_harvester_yearsplit_parameters import *
+else:
+       from harvester_yearsplit_parameters import *  
+
+global automatic_call
+automatic_call = False
+if station_config_dir != '.':
+       automatic_call = True
 
 
 ### make list of stations to run 
@@ -33,15 +68,21 @@ all_stations  ={}
 for db in datasets:
 
        # total number of files in the original datasets 
-       file_df = get_all_stations_files(db, station_kind)
- 
+       if automatic_call:
+              file_df = get_all_stations_files(db, station_kind, station_config_dir)
+              dsp = datasets_path[datasets[0]]
+              available = [i for i in file_df.file if os.path.isfile(dsp+'/'+i)]
+              file_df = file_df[file_df.file.isin(available)]
+       else:
+              file_df = get_all_stations_files(db, station_kind)
+       
        # select stations from a list 
        if stations:
               print("+++ Only running selected stations::: " , stations )
               file_df =file_df.loc[file_df.primary_id.isin(stations) ]       
 
-       # # DEBUGGING: to run only a single station:
-       # file_df = file_df[file_df['primary_id'].str.contains('37717')]
+       # # # DEBUGGING: to run only a single station:
+       # file_df = file_df[file_df['primary_id'].str.contains('11035|484848')]
 
        ## extracting fully harvested files i.e. all available years were harvested  
        ### these files will not be checked anymore against the year during the harvesting 
@@ -111,8 +152,8 @@ for db in datasets:
        for c in chunks:
               #print ('*** I am running CHUNK: ', chunks.index(c) , ' *** with: ' ,  len(c), ' files'  )
               c = str(','.join(c)).replace('#','')
-              command = 'python harvest_convert_to_netCDF_yearSplit.py' # changed from python3.8
-              com =  command + ' -d ' + db + ' -o ' + out_dir + ' -f  ' + c + '  -r ' + str(run_only_missing_stations) +   '  -k ' + station_kind + '  & '
+              command = f'python {ceuas_dir}/public/harvest/code_cop2/harvest_convert_to_netCDF_yearSplit.py' # changed from python3.8
+              com =  command + ' -d ' + db + ' -o ' + out_dir + ' -f  ' + c + '  -r ' + str(run_only_missing_stations) +   '  -k ' + station_kind + ' -pf ' + parameter_file + ' & '
               print(com)
               os.system( com )
               
