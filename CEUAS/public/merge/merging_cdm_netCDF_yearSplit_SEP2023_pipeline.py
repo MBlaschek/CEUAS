fix fstrings with wrong signs

CEUAS/public/merge/merging_cdm_netCDF_yearSplit_SEP2023_pipeline.py
@@ -426,13 +426,13 @@ class Merger():
                     
                     timestamps = data['recordtimestamp'][:]
                     if len(timestamps) != len(data['header_table']['latitude']):
-                        raise ValueError(f'{len(timestamps)} , {len(data['header_table']['latitude'])}')
+                        raise ValueError(f"{len(timestamps)} , {len(data['header_table']['latitude'])}")
                     #indices = data['recordindex'][:]
                     indices_inf = data['recordindex'][:] # starting index of the record
                     indices_sup = np.concatenate((data['recordindex'][:][1:], [data['observations_table']['index'].shape[0]]))
                 except Exception as e:
                     with open(os.path.expandvars('$HOME/corrupt.log'), 'a') as ff:
-                        ff.write(f'{str(datetime.now()).split('.')[0]}: {F}, {e}\n')
+                        ff.write(f"{str(datetime.now()).split('.')[0]}: {F}, {e}\n")
                     continue
                         
                 #data.[ indices_inf[1:][i] for i in  range(len(indices_inf[:-1])) ] # ending index of the record

CEUAS/public/resort/convert_and_resort_pipeline.py
@@ -1273,12 +1273,12 @@ def lin4d(fpattern,fns, out, p,pn, fdict,ts, ori, obstype, latorig,lonorig,z, re
         #print(os.path.basename(fns).split('_')[0], os.path.basename(fn), tpi.shape, time.time()-tt)
         if ans[1] == 7:
             
-            ptime(f'before {os.path.basename(fns).split('_')[0]} {os.path.basename(fn)}', tt)
+            ptime(f"before {os.path.basename(fns).split('_')[0]} {os.path.basename(fn)}", tt)
         idx = triint(tpi, reatab, tlon, tlat, lons, lats, secs, tss,ori, obss,pri,zs, pn, dy, fns, 5)
         
         if ans[1] == 7:
             
-            ptime(f'after {os.path.basename(fns).split('_')[0]} {os.path.basename(fn)}', tt)
+            ptime(f"after {os.path.basename(fns).split('_')[0]} {os.path.basename(fn)}", tt)
             #ptime(f'after {os.path.basename(fns).split('_')[0]} {os.path.basename(fn)}, {np.abs(np.nanmax(reatab[:idx.shape[0]])):.4f} {np.nanmin(reatab[:idx.shape[0]]):.4f}', tt)
     except FileNotFoundError as e:
         print(fn, 'not available, continuing')
@@ -2457,8 +2457,8 @@ def retrieve_anfg(f, out, out_fb, readict, ts, tsu, tslice, refs, gactor, out_na
                         fak *= 1000
                     if r2 == 0.:
                         r2 = np.nan
-                    print(f'{os.path.basename(fn).split('_C')[0]} {fps[1].filename.split('/')[-1]} {plevs[i]} hPa, ratio {r1/r2:.4f} {n1},{n2}; offline '+\
-                          f'{r1 * fak:.4f} online {r2 * fak:.4f}')
+                    print(f"{os.path.basename(fn).split('_C')[0]} {fps[1].filename.split('/')[-1]} {plevs[i]} hPa, ratio {r1/r2:.4f} {n1},{n2}; offline "+\
+                          f"{r1 * fak:.4f} online {r2 * fak:.4f}")
                 
                 
                 readict[k][dtype][p] = {}
@@ -2585,7 +2585,7 @@ def read_station_height(fn, jj, inventory_list, pd_list=[]):
                     #heights[:] = pd_list[-1]['Hha'][l]
                     #print(heights[0])
                     #print(pd_list[-1]['start_date'][l], pd_list[-1]['elevation'][l])
-                    print(f'{wigos} height: {pd_list[-1]['elevation'][l]}')
+                    print(f"{wigos} height: {pd_list[-1]['elevation'][l]}")
                     heights[:] = pd_list[-1]['elevation'][l]
             except Exception as e:
                 print(fns, e)
@@ -2601,7 +2601,7 @@ def read_station_height(fn, jj, inventory_list, pd_list=[]):
                     #heights[:] = pd_list[-1]['Hha'][l]
                     #print(heights[0])
                     #print(pd_i['start_date'][l], pd_i['elevation'][l])
-                    print(f'{wigos} height: {pd_list[-1]['elevation'][l]}')
+                    print(f"{wigos} height: {pd_list[-1]['elevation'][l]}")
                     heights[:] = pd_i['elevation'][l]
             except Exception as e:
                 print(fns, e)